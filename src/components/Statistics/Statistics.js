ce0ee486dd6f3721b73cfc90bcd095c3daa6984c
SebastianAdamoszek <sebaadamoszek@vp.pl>
2023-11-14 21:20:19 +0100

    corrected location utils

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Statistics.module.css';
-import { getRandomColor } from '../utils/getRandomColor'
+import { getRandomColor } from '../../utils/getRandomColor'
 
 const Statistics = ({ title, stats }) => {
   return (
